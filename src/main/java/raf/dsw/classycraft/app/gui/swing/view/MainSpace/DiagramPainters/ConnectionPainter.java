1e71ff217c6af84da68e79f59f2bb39029bf8f6c
lonskyne <lonskyne@gmail.com>
2023-12-06 01:28:28 +0100

    Generalization connection now gets properly drawn.

--- src/main/java/raf/dsw/classycraft/app/gui/swing/view/MainSpace/DiagramPainters/ConnectionPainter.java
+++ src/main/java/raf/dsw/classycraft/app/gui/swing/view/MainSpace/DiagramPainters/ConnectionPainter.java
@@ -2,6 +2,7 @@ package raf.dsw.classycraft.app.gui.swing.view.MainSpace.DiagramPainters;
 
 import raf.dsw.classycraft.app.core.ProjectTreeAbstraction.DiagramAbstraction.Connection;
 import raf.dsw.classycraft.app.core.ProjectTreeAbstraction.DiagramAbstraction.InterClass;
+import raf.dsw.classycraft.app.core.ProjectTreeImplementation.DiagramImplementation.Connections.Generalization;
 
 import java.awt.*;
 import java.awt.geom.GeneralPath;
@@ -21,6 +22,8 @@ public class ConnectionPainter extends DiagramElementPainter {
 
         InterClass to = connection.getTo();
         InterClass from = connection.getFrom();
+
+        diagramElement = connection;
 
         ArrayList<Point2D> toPoints = new ArrayList<Point2D>();
         toPoints.add(new Point2D.Double(to.getPosition().getX() + to.getSize().getWidth()/2, to.getPosition().getY()));
@@ -53,7 +56,66 @@ public class ConnectionPainter extends DiagramElementPainter {
 
     @Override
     public void paint(Graphics2D g) {
-        g.drawLine((int)fromPointMin.getX(), (int)fromPointMin.getY(), (int)toPointMin.getX(), (int)toPointMin.getY());
+        //g.drawLine((int)fromPointMin.getX(), (int)fromPointMin.getY(), (int)toPointMin.getX(), (int)toPointMin.getY());
+
+        if(diagramElement instanceof Generalization) {
+            double triangleA = 20;
+
+            g.draw(drawTriangle(new Point2D.Double((double)toPointMin.getX(), (double)toPointMin.getY()), triangleA));
+
+            g.drawLine((int)fromPointMin.getX(), (int)fromPointMin.getY(), (int)getC(triangleA).getX(), (int)getC(triangleA).getY());
+        }
+    }
+
+    private Point2D getC(double a) {
+        double x1 = fromPointMin.getX(), y1 = fromPointMin.getY(), x2 = toPointMin.getX(), y2 = toPointMin.getY();
+        double h = a * Math.sqrt(3) / 2;
+        double AB = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+        double xC = (x1 + ((AB-h)/h) * x2) / (1 + (AB-h)/h);
+        double yC = (y1 + ((AB-h)/h) * y2) / (1 + (AB-h)/h);
+
+        return new Point2D.Double(xC, yC);
+    }
+
+    private Shape drawTriangle(Point2D begPoint, double a) {
+        Shape shape = new GeneralPath();
+
+        ((GeneralPath)shape).moveTo(begPoint.getX(), begPoint.getY());
+
+        double x1 = fromPointMin.getX(), y1 = fromPointMin.getY(), x2 = toPointMin.getX(), y2 = toPointMin.getY();
+
+        double xC = getC(a).getX();
+        double yC = getC(a).getY();
+
+        double k = (y2 - yC) / (x2 - xC);
+        double kn = (xC - x2) / (y2 - yC);
+        double kl = (3 * k - Math.sqrt(3)) / (Math.sqrt(3) * k + 3);
+
+        double n = yC - kn * xC;
+        double nl = y2 - kl * x2;
+        double t = kn / kl;
+
+        double newY1 = (n - t * nl) / (1 - t);
+        double newX1 = (newY1 - nl) / kl;
+
+        ((GeneralPath)shape).lineTo(newX1, newY1);
+
+        kl = (-3 * k - Math.sqrt(3)) / (Math.sqrt(3) * k - 3);
+
+        nl = y2 - kl * x2;
+        t = kn / kl;
+
+        double newY2 = (n - t * nl) / (1 - t);
+        double newX2 = (newY2 - nl) / kl;
+
+        ((GeneralPath)shape).lineTo(newX2, newY2);
+
+        ((GeneralPath)shape).lineTo(begPoint.getX(), begPoint.getY());
+
+        ((GeneralPath) shape).closePath();
+
+        return shape;
     }
 
     @Override
