5bc8d5be1165b60ebde850a548941ce007771763
MrVector03 <60327085+MrVector03@users.noreply.github.com>
2023-12-14 00:20:27 +0100

    Increased initial window size.

--- src/main/java/raf/dsw/classycraft/app/gui/swing/view/MainFrame.java
+++ src/main/java/raf/dsw/classycraft/app/gui/swing/view/MainFrame.java
@@ -74,7 +74,7 @@ public class MainFrame extends JFrame implements ISubscriber {
         Dimension screenSize = kit.getScreenSize();
         int screenHeight = screenSize.height;
         int screenWidth = screenSize.width;
-        setSize(screenWidth / 2, screenHeight / 2);
+        setSize(screenWidth / 2, screenHeight / 2 + 50);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setTitle("ClassyCrafT");
